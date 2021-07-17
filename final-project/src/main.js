bb149158e7fd9e5e18359eb5d2962b0ceefee266
April <64.aprilhill@gmail.com>
2021-07-16 18:45:02 -0600

    Changed the page's display to grid. There are two columns for medium and large screens, one column for small screens.  Changed the header to have a slightly more modern look. Made it larger and not sticky. Rearranged the styling in the media queries.

---
+++ final-project/src/main.js
@@ -0,0 +1,16 @@
+/*
+window.addEventListener("scroll", function() {});
+
+
+function showFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("zen-explanation").style.visibility = "visible";
+        document.getElementById("zen-explanation").style.animation = "fadeEffect 3s";
+        // console.log("> 300");
+    } else {
+        document.getElementById("zen-explanation").style.visibility = "hidden";
+        document.getElementById("zen-explanation").style.animation = "none";
+        // console.log("< 300");
+    }
+}
+*/
